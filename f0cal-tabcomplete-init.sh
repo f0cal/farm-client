84473aea16ed4a45bc6700744661c2feb5dcbab2
quincy2112 <qphiwum@gmail.com>
2021-02-04 23:40:21 -0500

    Change imports back to "import f0cal.core"
    Fix import in farm client generated code

--- f0cal-tabcomplete-init.sh
+++ f0cal-tabcomplete-init.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 ACTIVATE=$VIRTUAL_ENV/src/plugnparse/activate-argcomplete.sh
 if ! grep -q "source "$ACTIVATE $VIRTUAL_ENV/bin/activate; then
   printf "\n\n# activates f0cal namespace tab completion
-source "$ACTIVATE >> $VIRTUAL_ENV/bin/activate
+source $ACTIVATE" >> $VIRTUAL_ENV/bin/activate
 fi
-source $ACTIVATE
+source $VIRTUAL_ENV/src/plugnparse/activate-argcomplete.sh
